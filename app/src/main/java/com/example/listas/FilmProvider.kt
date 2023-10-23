92329ccd45af330703a27bc5efa77fcaae78a566
Jesuslavado <kjblpo@gmail.com>
2023-10-23 11:13:39 +0200

    Listas

---
+++ app/src/main/java/com/example/listas/FilmProvider.kt
@@ -0,0 +1,34 @@
+package com.example.listas
+
+class FilmProvider {
+    companion object {
+        val filmList= listOf<Film>(
+            Film(
+                title="lighyear",
+                director="director",
+                year=2022,
+                photo="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FDisney-Pixar-Lightyear-P%25C3%25B3ster-pared%2Fdp%2FB09Y9G9DRN&psig=AOvVaw2YZti53-Sy49X4vmdE-YTh&ust=1698137902114000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICy9-Tmi4IDFQAAAAAdAAAAABAE"
+            ),
+            Film(
+                title="cars",
+                director="director",
+                year=2006,
+                photo="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.posters.es%2Fposters%2Fcars-one-sheet-v1254&psig=AOvVaw0Lb9XQI49IelukzFDzb1RA&ust=1698138220154000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIjA2Pzni4IDFQAAAAAdAAAAABAE"
+            ),
+            Film(
+                title="lighyear",
+                director="director",
+                year=2022,
+                photo="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FDisney-Pixar-Lightyear-P%25C3%25B3ster-pared%2Fdp%2FB09Y9G9DRN&psig=AOvVaw2YZti53-Sy49X4vmdE-YTh&ust=1698137902114000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICy9-Tmi4IDFQAAAAAdAAAAABAE"
+            ),
+            Film(
+                title="lighyear",
+                director="director",
+                year=2022,
+                photo="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FDisney-Pixar-Lightyear-P%25C3%25B3ster-pared%2Fdp%2FB09Y9G9DRN&psig=AOvVaw2YZti53-Sy49X4vmdE-YTh&ust=1698137902114000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCICy9-Tmi4IDFQAAAAAdAAAAABAE"
+            )
+        )
+
+
+    }
+}
---
+++ app/src/main/java/com/example/listas/adapted/FilmViewHolder.kt
@@ -0,0 +1,4 @@
+package com.example.listas.adapted
+
+class FilmViewHolder {
+}
